6f8a12d9999554b960f832da63a5267414f03def
Stefan <shodgeskluck@gmail.com>
2019-12-24 17:05:14 -0700

    Updating root module

--- ParkMyBike.App/ClientApp/src/app/app.module.ts
+++ ParkMyBike.App/ClientApp/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ModalModule } from "ngx-bootstrap";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BikeracksComponent } from './bikeracks/bikeracks.component';
+import { BikeRacksComponent } from './bikeracks/bikeracks.component';
 import { BikeRacksService } from './services/bikeracks.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateBikerackModalComponent } from './bikeracks/create-bikerack-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BikeracksComponent
+    BikeRacksComponent,
+    CreateBikerackModalComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     BikeRacksService
