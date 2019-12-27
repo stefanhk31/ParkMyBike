7103aafaca683587f8fafa4d6a39efa92d717b93
Stefan <shodgeskluck@gmail.com>
2019-12-27 11:21:52 -0700

    Creating create bikerack form

--- ParkMyBike.App/ClientApp/src/app/bikeracks/create-bikerack-modal.component.ts
+++ ParkMyBike.App/ClientApp/src/app/bikeracks/create-bikerack-modal.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap';
 import { BikeRack } from '../interfaces/bikerack.interface';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { BikeRacksService } from '../services/bikeracks.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Statuses, RackTypes } from '../resources/enums';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'create-bikerack-modal',
@@ -12,13 +15,16 @@ export class CreateBikerackModalComponent implements OnInit {
   @ViewChild('createBikeRackModal', { static: true }) modal: ModalDirective;
   @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
 
+  statuses: string[] = Object.values(Statuses).filter((x => isNaN(Number(x)))) as string[];
+  types: string[] = Object.values(RackTypes).filter((x => isNaN(Number(x)))) as string[];
+
   bikeRack: BikeRack = {
-    id: 0,
-    numberOfRacks: 0,
-    latLong: "",
-    locationDescription: "",
-    status: "",
-    rackType: ""
+    RackId: 0,
+    NumberOfRacks: 0,
+    LatLong: "",
+    LocationDescription: "",
+    Status: "",
+    RackType: ""
   };
 
   active = false;
@@ -26,9 +32,27 @@ export class CreateBikerackModalComponent implements OnInit {
 
   newBikeRackForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private bikeRacksService: BikeRacksService,
+    private formBuilder: FormBuilder
+    ) {}
 
   ngOnInit(): void {
+    this.newBikeRackForm = this.formBuilder.group({
+      NumberOfRacks: [],
+      LatLong: [],
+      LocationDescription: [],
+      Status: [],
+      RackType: []
+    });
+  }
+
+  mapFormValueToBikeRackType(form: FormGroup): void {
+    this.bikeRack.NumberOfRacks = form.value["NumberOfRacks"];
+    this.bikeRack.LatLong = form.value["LatLong"];
+    this.bikeRack.LocationDescription = form.value["LocationDescription"];
+    this.bikeRack.Status = form.value["Status"];
+    this.bikeRack.RackType = form.value["RackType"];
   }
 
   show(): void {
@@ -38,6 +62,13 @@ export class CreateBikerackModalComponent implements OnInit {
 
   save(): void {
     this.saving = true;
+    this.mapFormValueToBikeRackType(this.newBikeRackForm);
+    this.bikeRacksService.addBikeRack(this.bikeRack)
+    .pipe(finalize(() => { this.saving = false;}))
+    .subscribe(() => {
+      this.close();
+      this.modalSave.emit(null);
+    });
   }
 
   close(): void {
