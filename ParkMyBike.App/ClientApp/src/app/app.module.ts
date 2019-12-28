d8ea7e8f324848f18ced7bfd9826ae0ac21b02f5
Stefan <shodgeskluck@gmail.com>
2019-12-28 16:44:55 -0700

    Adding sweetAlert to project

--- ParkMyBike.App/ClientApp/src/app/app.module.ts
+++ ParkMyBike.App/ClientApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ModalModule } from "ngx-bootstrap";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,7 +25,8 @@ import { EditBikerackModalComponent } from './bikeracks/edit-bikerack-modal.comp
     HttpClientModule,
     ModalModule.forRoot(),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SweetAlert2Module.forRoot()
   ],
   providers: [
     BikeRacksService
