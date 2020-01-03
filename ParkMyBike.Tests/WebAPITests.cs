613e14fb6e5c94054e693f1ea0ce436c350d403b
Stefan <shodgeskluck@gmail.com>
2020-01-02 21:26:56 -0500

    Updating webAPI tests to reflect service updates
    Deleting obsolete update methods
    Changing delete method test

--- ParkMyBike.Tests/WebAPITests.cs
+++ ParkMyBike.Tests/WebAPITests.cs
@@ -67,59 +67,11 @@ namespace ParkMyBike.Tests
         }
 
         [Fact]
-        public void UpdateNumberOfRacksReturnsSuccess()
-        {
-            var rack = GenerateTestBikeRack(1);
-            _repository.AddBikeRack(rack);
-            ActionResult<BikeRack> result = _controller.UpdateNumberOfRacks(1, 3);
-            var contentResult = new OkObjectResult(result);
-
-            Assert.NotNull(contentResult);
-            Assert.Equal(200, contentResult.StatusCode);
-        }
-
-        [Fact]
-        public void UpdateLocationDescriptionReturnsSuccess()
-        {
-            var rack = GenerateTestBikeRack(1);
-            _repository.AddBikeRack(rack);
-            ActionResult<BikeRack> result = _controller.UpdateLocationDescription(rack.Id, "A new description");
-            var contentResult = new OkObjectResult(result);
-
-            Assert.NotNull(contentResult);
-            Assert.Equal(200, contentResult.StatusCode);
-        }
-
-        [Fact]
-        public void UpdateRackStatusReturnsSuccess()
-        {
-            var rack = GenerateTestBikeRack(1);
-            _repository.AddBikeRack(rack);
-            ActionResult<BikeRack> result = _controller.UpdateRackStatus(rack.Id, RackStatus.ToBeReplaced);
-            var contentResult = new OkObjectResult(result);
-
-            Assert.NotNull(contentResult);
-            Assert.Equal(200, contentResult.StatusCode);
-        }
-
-        [Fact]
-        public void UpdateRackTypeReturnsSuccess()
-        {
-            var rack = GenerateTestBikeRack(1);
-            _repository.AddBikeRack(rack);
-            ActionResult<BikeRack> result = _controller.UpdateRackType(rack.Id, RackType.Hoop);
-            var contentResult = new OkObjectResult(result);
-
-            Assert.NotNull(contentResult);
-            Assert.Equal(200, contentResult.StatusCode);
-        }
-
-        [Fact]
         public void DeleteReturnsSuccess()
         {
             var rack = GenerateTestBikeRack(1);
             _repository.AddBikeRack(rack);
-            ActionResult<BikeRack> result = _controller.Delete(_mapper.Map<BikeRack, BikeRackViewModel>(rack));
+            ActionResult<BikeRack> result = _controller.Delete(rack.Id);
             var contentResult = new OkObjectResult(result);
 
             Assert.NotNull(contentResult);
