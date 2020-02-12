4442482076f333c05ba7e978e5118f3041d85bdc
Stefan <shodgeskluck@gmail.com>
2020-02-11 20:41:18 -0500

    Adding more test coverage to angular app

---
+++ ParkMyBike.App/ClientApp/src/app/resources/testResources.ts
@@ -0,0 +1,22 @@
+import { BikeRack } from '../interfaces/bikerack.interface';
+
+let mockBikeRacks: BikeRack[] = [
+    {
+        rackId: 0,
+        numberOfRacks: 0,
+        latLong: '',
+        locationDescription: '',
+        status: '',
+        rackType: ''
+    },
+    {
+        rackId: 0,
+        numberOfRacks: 0,
+        latLong: '',
+        locationDescription: '',
+        status: '',
+        rackType: ''
+    }
+];
+
+export default mockBikeRacks;
